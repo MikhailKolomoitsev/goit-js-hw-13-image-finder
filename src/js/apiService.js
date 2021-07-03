becd13582d4be743ccd649161f59675b35a6a487
mihon.testosterone <mihon.testosterone@gmail.com>
2021-07-03 17:33:30 +0300

    first_ideas

---
+++ src/js/apiService.js
@@ -0,0 +1,5 @@
+let pageNumber='';
+let searchQuery = ''
+const key=22343413-b046050a5932d0d712f3b0fd0
+const baseUrl = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q`
+// =${searchQuery}&page=${pageNumber}&per_page=12
