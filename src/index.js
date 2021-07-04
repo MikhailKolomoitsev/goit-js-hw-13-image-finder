2a2f8ffe2c2986695713ea311f85705548d3cea3
mihon.testosterone <mihon.testosterone@gmail.com>
2021-07-04 20:50:17 +0300

    Something more

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './sass/main.scss';
+import card from './temp/card.hbs'
+import './js/apiService'
+import './js/load-more-btn'
+import '@pnotify/core/dist/BrightTheme.css';
+import apiService from './js/apiService';
+const { error } = require('@pnotify/core');
+var debounce = require('lodash.debounce');
+
+const fetchCardData = new apiService
+
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    articlesContainer: document.querySelector('.gallery')
+}
+
+refs.searchFrom.addEventListner('sumbit', debounce(onSearch, 1000))
+refs.searchFrom.addEventListner('click', clearSearchForm)
+
+function onSearch(e) {
+    e.preventDefalut()
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,33 @@
-let pageNumber='';
-let searchQuery = ''
 const key=22343413-b046050a5932d0d712f3b0fd0
 const baseUrl = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q`
-// =${searchQuery}&page=${pageNumber}&per_page=12
+
+export default class apiService{
+    constructor() {
+        this.pageNumber = 1
+        this.searchQuery=''
+    }
+    
+    async fetchRequest(){
+    try {
+        let res = await fetch(`${baseUrl}=${this.searchQuery}&page=${this.pageNumber}&per_page=12&key=${key}`)
+        let data = await res.json()
+        this.incrementPagesNumber()
+        return data
+    } catch(e) {
+        console.log('error:' e);
+    }
+    }
+    get queryValue() {
+        return this.searchQuery;
+    }
+    set queryValue(value) {
+        this.searchQuery = value;
+    }
+    incrementPagesNumber() {
+        this.pageNumber+=1
+    }
+     resetPagesNumber() {
+        this.pageNumber = 1;
+    }
+
+}
